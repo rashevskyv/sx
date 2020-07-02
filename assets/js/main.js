1eb7fb68030df0d4228c95bcd425f31111bf9cfa
rashevskyv <rashevsky.v@gmail.com>
2020-07-02 20:59:54 +0300

    bugfix

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,16 +69,4 @@ $(document).ready(function(){
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
-  $(function () {
-    $('.popup-modal').magnificPopup({
-      type: 'inline',
-      preloader: false,
-      focus: '#username',
-      modal: true
-    });
-    $(document).on('click', '.popup-modal-dismiss', function (egg) {
-      egg.preventDefault();
-      $.magnificPopup.close();
-    });
-  });
 });
